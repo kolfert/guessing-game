9c2dc4d8da138c92976653807a72123e6d3d7b44
Kevin Olfert <kolfert@arrisi.com>
2021-02-13 08:51:56 -1000

    Added a loop, and better error handeling

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -8,20 +8,33 @@ fn main()
     println!("Guess a number!");
 
     let secret_num = rand::thread_rng().gen_range(0,100);
-    println!("The secret number is : {}", secret_num);
+    //println!("The secret number is : {}", secret_num);
 
-    println!("Input your guess, numbers only, 0-100");
-    let mut guess = String::new();
+    loop {
+        println!("Input your guess, numbers only, 0-100");
+        let mut guess = String::new();
 
-    io::stdin().read_line(&mut guess).expect("Failed to read line");
-    let guess :u32 = guess.trim().parse().expect("Please type a number!");
+        io::stdin()
+            .read_line(&mut guess)
+            .expect("Failed to read line");
 
-    println!("You guessed: {}", guess);
+        let guess: u32 = match guess.trim().parse()
+        {
+            Ok(num) => num,
+            Err(_)  => continue,
+        };
 
-    match guess.cmp(&secret_num) 
-    {
-        Ordering::Less => println!("Too small!"),
-        Ordering::Greater => println!("Too big!"),
-        Ordering::Equal => println!("You win!"),
+        println!("You guessed: {}", guess);
+
+        match guess.cmp(&secret_num) 
+        {
+            Ordering::Less => println!("Too small!"),
+            Ordering::Greater => println!("Too big!"),
+            Ordering::Equal => 
+            {
+                    println!("You win!");
+                    break;
+            }
+        }
     }
 }
