9556bb577386bd531b8a08eba2c7adcc0831756b
Kevin Olfert <kolfert@arrisi.com>
2021-02-12 15:43:16 -1000

    Added number comparison code

--- guessing_game/src/main.rs
+++ guessing_game/src/main.rs
@@ -1,5 +1,6 @@
 use std::io;
 use rand::Rng;
+use std::cmp::Ordering;
 
 
 fn main() 
@@ -12,9 +13,15 @@ fn main()
     println!("Input your guess, numbers only, 0-100");
     let mut guess = String::new();
 
-    io::stdin()
-        .read_line(&mut guess)
-        .expect("Failed to read line");
+    io::stdin().read_line(&mut guess).expect("Failed to read line");
+    let guess :u32 = guess.trim().parse().expect("Please type a number!");
 
     println!("You guessed: {}", guess);
+
+    match guess.cmp(&secret_num) 
+    {
+        Ordering::Less => println!("Too small!"),
+        Ordering::Greater => println!("Too big!"),
+        Ordering::Equal => println!("You win!"),
+    }
 }
